e76e2b930d422b387061277a745d1a5d061b1a80
danielf <daniel.fela@e-systemy.com>
2023-06-12 09:51:05 +0200

    rek update

--- app/config/services.php
+++ app/config/services.php
@@ -58,29 +58,6 @@ $di->setShared('url', function () {
 });
 
 /**
- * Crypt service
- */
-/*$di->set('tag', function () use ($config, $di) {
-    $tag = new \Phalcon\Tag();
-    $tag->setDi($di);
-    return $tag;
-}, true);*/
-
-/**
- * JWT service
- */
-/*$di->setShared('jwt', function () {
-    return new JWT();
-});*/
-
-/**
- * tokenConfig
- */
-/*$di->setShared('tokenConfig', function () use ($config) {
-    return $config->authentication->toArray();
-});*/
-
-/**
  * Database connection is created based in the parameters defined in the configuration file
  */
 $di->setShared('db', function () {
@@ -99,41 +76,8 @@ $di->setShared('db', function () {
     return $connection;
 });
 
-/*$di->setShared('cache', function () {
-    return new Library\Cache\Adapter();
-});
-
-$di->setShared('translate', function () {
-    return new Library\Translation\Adapter();
-});*/
-/*
-$di->set('helper', new \Library\Support\HelperFactory());*/
-
 $di->setShared('request', new  Phalcon\Http\Request());
 $di->setShared('response', new  Phalcon\Http\Response());
-
-/*$di->set('session', function () {
-    $maxlifetime = 0;
-    session_name(SESSION_NAME);
-    session_set_cookie_params([
-        'lifetime' => $maxlifetime,
-        'path' => '/',
-        'domain' => '.daniel-fela.pl',
-        'secure' => false,
-        'httponly' => false,
-        'samesite' => 'lax'
-    ]);
-
-    $session = new Manager();
-    $files = new Stream(['savePath' => '/tmp',]);
-
-    $session->setId('test')//REMOVE BEFORE SESSION RUN!!!!!!!!!!!
-        ->setAdapter($files)->start();
-
-    $session->set('discord-request-count', 0);
-
-    return $session;
-});*/
 
 $di->set('modelsCache', function () {
 
